630afffc31ad42191cf2483a5c7ad33d8bfc2dd9
flaviorv <frezendevaroto@gmail.com>
2025-02-04 20:51:22 -0300

    Comparisons between sequential and parallel addition of large vector elements

---
+++ tp2/q1.2/chart.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import json
+
+def generate_chart():
+    data = None
+    with open("./times.json", "r") as f:
+        res = json.load(f)
+        data = res
+
+    for len in data: 
+        title = f"Summing {int(len):,} numbers of a vector"
+        names = []
+        times = []
+
+        for name in data[len]:
+            times.append(data[len][name])
+            names.append(name)
+
+        plt.bar(names, times)
+        plt.xlabel("Cost of sums and generating lists")
+        plt.ylabel("Seconds")
+        plt.title(title)
+        plt.savefig(f"s{len}.png")
+
+
+if __name__ == "__main__":
+    generate_chart()
