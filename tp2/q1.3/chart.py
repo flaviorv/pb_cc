a1e8da475649e36586b438023752adffb4409c59
flaviorv <frezendevaroto@gmail.com>
2025-02-05 12:08:15 -0300

    Editing images in asynchronous way. Using different numbers of workers to do the same thing and comparing the time.

---
+++ tp2/q1.3/chart.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import json
+
+def create_chart():
+    data = None
+    with open("./times.json", "r") as f:
+        data = json.load(f)
+
+    times = []
+    threads = [] 
+    for d in data:
+        threads.append(d["threads"])
+        times.append(d["seconds"])
+
+    plt.plot(threads, times)
+    plt.xlabel("Threads")
+    plt.ylabel("Seconds")
+    plt.title("Runtime of filter applying")
+    plt.savefig("times_chart.png")
+
+
+if __name__ == "__main__":
+    create_chart()
