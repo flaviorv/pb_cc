66920805b5266b4ed699b1b204dc82095de87161
flaviorv <frezendevaroto@gmail.com>
2025-02-06 14:15:56 -0300

    There was confusion between questions 2.1 and 2.2; the question 2.1 is finished now (Charts were created to compare the execution times of quick sort unsing many types of pivots.

---
+++ tp2/q2.1/chart.py
@@ -0,0 +1,23 @@
+from rw_files import get
+import matplotlib.pyplot as plt
+
+
+def generate_chart():
+    data = get("quick_sort_average.json")
+
+    for d in data:
+        title = f"Quick sort receiving an {d["arr_type"]} array"
+        xlabel = "Type of pivots"
+        ylabel = "Seconds"
+
+        pivots = ["start",  "middle", "end", "random"]
+        seconds = [d["start"], d["middle"], d["end"], d["random"]]
+
+        plt.bar(pivots, seconds)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.savefig(f"{d["arr_type"]}_times.png")
+
+if __name__ == "__main__":
+    generate_chart()
--- tp2/q2.1/samples_average.py
+++ tp2/q2.1/samples_average.py
@@ -1,31 +1,30 @@
-from rw_files import get, save
+from rw_files import save, get
 
 def generate_average_time(samples):
-    starts = 0
-    middles = 0
-    ends = 0
-    randoms = 0
-
     data = get("quick_sort_times.json")
-   
+
+    unordered = {"arr_type": "unordered", "start": 0, "middle": 0, "end": 0, "random": 0}
+    ordered = {"arr_type": "ordered", "start": 0, "middle": 0, "end": 0, "random": 0}
+    inverse = {"arr_type": "inverse", "start": 0, "middle": 0, "end": 0, "random": 0}
+
+    average_data = [unordered, ordered, inverse]
+
     for d in data:
+        arr_type = d["arr_type"]
         pivot_type = d["pivot_type"]
-        match(pivot_type):
-            case "start":
-                starts += d["sec"]
-            case "middle":
-                middles += d["sec"]                   
-            case "end":
-                ends += d["sec"]
-            case "random":
-                randoms += d["sec"]
+        sec = d["sec"]
+        match(arr_type):
+            case "unordered":
+                unordered[pivot_type] += sec
+            case "ordered":
+                ordered[pivot_type] += sec
+            case "inverse":
+                inverse[pivot_type] += sec
 
-    start_average = {"pivot_type": "start", "sec": (starts / samples)}
-    middle_average = {"pivot_type": "middle", "sec": middles / samples}
-    end_average = {"pivot_type": "end", "sec": ends / samples}
-    random_average = {"pivot_type": "random", "sec": randoms / samples}
+    for a in average_data:
+        a["start"] = round(a["start"] / samples, 4)
+        a["middle"] = round(a["middle"] / samples, 4)
+        a["end"] = round(a["end"] / samples, 4)
+        a["random"] = round(a["random"] / samples, 4)
 
-
-    save([start_average, middle_average, end_average, random_average], "quick_sort_average.json")
-        
-generate_average_time(3)
+    save(average_data, "quick_sort_average.json")
