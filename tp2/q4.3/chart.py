866b6fa5e32e99f691da0d10e1bd8d04d799fea7
flaviorv <frezendevaroto@gmail.com>
2025-02-21 16:53:11 -0300

    Torre de Hanoi steps, time complexity and chart.

---
+++ tp2/q4.3/chart.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import json
+
+def get_data():
+    with open("times.json", "r") as f:
+        json_content = f.read()
+        content = json.loads(json_content)
+        return content
+
+
+def get_arranged_data():
+    times = []
+    discs = []
+    content = get_data()
+    for c in content:
+        times.append(c["time"])
+        discs.append(c["discs"])
+    return [discs, times]
+
+
+def generate_chart():
+    data = get_arranged_data()
+    plt.plot(data[0], data[1])
+    plt.title("Torre de Hanói - Tempo exponencial - O(2ⁿ)")
+    plt.xlabel("Discos")
+    plt.ylabel("Segundos")
+    plt.savefig("times_chart.png")
+
+
+if __name__ == "__main__":
+    generate_chart()
---
+++ tp2/q4.3/save_times.py
@@ -0,0 +1,24 @@
+from time import time
+from hanoi_tower import init
+import json
+
+def __check_time(discs):
+    start = round(time(), 3)
+    init(discs)
+    end = round(time(), 3)
+    t = round(end - start , 3) 
+    return {"discs": discs, "movements": (2**discs -1), "time": t}
+
+
+def save_times(discs_sequence):
+    times = []
+    for disc in range(1, discs_sequence+1):
+        times.append(__check_time(disc))
+    
+    content = json.dumps(times, indent=2)
+    with open("times.json", "w") as f:
+        f.write(content)
+        
+
+if __name__ == "__main__":
+    save_times(23)
