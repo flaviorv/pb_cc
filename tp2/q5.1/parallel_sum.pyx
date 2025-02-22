1b03c92ec3fe32d552869a94d11cb5af734ec9c8
flaviorv <frezendevaroto@gmail.com>
2025-02-21 22:28:11 -0300

    Summing the vector elements in parallel and sequential way, comparing the times, generate charts with several inputs.

---
+++ tp2/q5.1/parallel_sum.pyx
@@ -0,0 +1,34 @@
+#cython: boundscheck=False
+#cython: wraparound=False
+from cython.parallel import prange
+import numpy as np
+cimport numpy as np
+import time
+from random import randrange
+
+def sum(unsigned long size, unsigned long max_range):
+    cdef unsigned long i
+    cdef unsigned long result
+    result = 0
+    content = {}
+
+    start = round(time.time(), 3)
+    cdef long long[:] numbers = np.random.randint(1, max_range, size=size)
+    end = round(time.time(), 3)
+    sec = end-start
+    name = "numpy-pyx"
+    print(f"{name} took {sec} seconds (pyx file)")
+    content[name] = sec
+
+
+    start = round(time.time(), 3)
+    with nogil:
+        for i in prange(size, schedule="guided"):
+            result += numbers[i]
+    end = round(time.time(), 3)
+    name = "sum-parallel"
+    sec = end-start
+    print(f"{name} took {sec} seconds and the result is {result} (pyx file)")
+    content[name] = sec
+    
+    return content
