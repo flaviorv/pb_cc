6181350de9e92d904a937dcee03b5345971cad9a
flaviorv <frezendevaroto@gmail.com>
2025-02-21 21:51:18 -0300

    Recursive permutation method that avoids repetitions.

---
+++ tp2/q4.4/permutation.py
@@ -0,0 +1,15 @@
+def permute(output, input):
+    if len(input) == 0:
+        print(output)
+    else:
+        memory = set()
+        for i in range(len(input)):
+            if input[i] in memory:
+                continue
+            memory.add(input[i])
+            permute(output + input[i], input[:i] + input[i+1:])
+
+#This recurive function to permute has time complexity O(n!)
+
+input = "ABCD"
+permute("", input)
