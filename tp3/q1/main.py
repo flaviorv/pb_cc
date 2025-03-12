ef99f600a4c9ae354faf265dd00981481f8899ce
flaviorv <frezendevaroto@gmail.com>
2025-03-12 08:52:54 -0300

    Search method to verify if exists a node with a input value. Check method to check if the binary tree is a binary search tree.

--- tp3/q1/main.py
+++ tp3/q1/main.py
@@ -7,7 +7,7 @@ class Node:
 class BinaryTree:
     def __init__(self):
         self.root = None
-    
+
     def add(self, value):
         if self.root is None:
             self.root = Node(value)
@@ -30,18 +30,18 @@ class BinaryTree:
         result = []
         self.__in_order(self.root, result)
         return result
-    
+
     def __in_order(self, current, result):
         if current is not None:
             self.__in_order(current.left, result)
             result.append(current.value)
             self.__in_order(current.right, result)
-    
+
     def pre_order(self):
         result = []
         self.__pre_order(self.root, result)
         return result
-    
+
     def __pre_order(self, current, result):
         if current is not None:
             result.append(current.value)
@@ -52,7 +52,7 @@ class BinaryTree:
         result = []
         self.__post_order(self.root, result)
         return result
-    
+
     def __post_order(self, current, result):
         if current is not None:
             self.__post_order(current.left, result)
@@ -67,7 +67,7 @@ class BinaryTree:
     def __remove(self, current, value, msg):
         if current is None:
             return current, msg
-        
+
         if value < current.value:
             current.left, msg = self.__remove(current.left, value, msg)
         elif value > current.value:
@@ -80,30 +80,72 @@ class BinaryTree:
                 return current.right, msg
             if current.right is None:
                 return current.left, msg
-            
+
             right_sub_min = self.__get_min(current.right)
             current.value = right_sub_min.value
             current.right, msg = self.__remove(current.right, right_sub_min.value, msg)
         return current, msg
-        
+
     def __get_min(self, current):
         while current.left is not None:
             current = current.left
         return current
+
+    def search(self, value):
+        print(f"Searching for {value}...")
+        msg = self.__search(self.root, value)
+        print(value, msg)
+
+    def __search(self, current, value):
+        if current is None:
+            return "not found"
+        if current.value == value:
+            return "found"
+        if value < current.value:
+            return self.__search(current.left, value)
+        else:
+            return self.__search(current.right, value)
+
+    def check(self):
+        is_bst = self.__check(self.root, True)
+        print(f"Is bst: {is_bst}")
+
+    def __check(self, current, is_bst):
+        if current != None:
+            if current.left is not None and current.left.value > current.value:
+                return False
+            elif current.right is not None and current.right.value < current.value:
+                return False
+            else:
+                is_bst = self.__check(current.left, is_bst)
+                is_bst = self.__check(current.right, is_bst)
+        return is_bst
 
 if __name__ == "__main__":
     bs = BinaryTree()
     nodes = [50, 30, 70, 20, 40, 60, 80]
     for node in nodes:
         bs.add(node)
-
+    bs.check()
     print("In order:", bs.in_order())
     print("Pre order:", bs.pre_order())
     print("Post order:", bs.post_order())
 
     bs.remove(20)
     print("Pre order:", bs.pre_order())
+    bs.check()
     bs.remove(30)
     print("Pre order:", bs.pre_order())
+    bs.check()
     bs.remove(50)
     print("Pre order:", bs.pre_order())
+    bs.check()
+
+    bs.search(40)
+    bs.search(50)
+
+    bs.root.left.left = Node(100)
+    bs.check()
+
+
+
