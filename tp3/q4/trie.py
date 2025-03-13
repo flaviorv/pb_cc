8dd0234f23dfce72f7f8aac6ad914c21d5ed7f18
flaviorv <frezendevaroto@gmail.com>
2025-03-13 20:18:37 -0300

    The binary method should converts only ipv6 and the find longest prefix match is fixed

--- tp3/q4/trie.py
+++ tp3/q4/trie.py
@@ -1,45 +1,101 @@
+import time, ipaddress
+from random import randrange
+
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.is_end_of_prefix = False
-        self.prefix = None
+        self.is_end = False
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
 
-    def insert(self, cidr):
-        ip, prefix_length = cidr.split('/')
-        prefix_length = int(prefix_length)
-        binary_ip = self._ip_to_binary(ip)[:prefix_length]
+    def insert(self, ip):
+        ip = self.convert_ipv6_to_binary(ip)
         node = self.root
-        for bit in binary_ip:
-            if bit not in node.children:
-                node.children[bit] = TrieNode()
-            node = node.children[bit]
-        node.is_end_of_prefix = True
-        node.prefix = cidr
+        for char in ip:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
 
-    def longest_prefix_match(self, ip):
-        binary_ip = self._ip_to_binary(ip)
+    def longest_prefix_match(self, prefix):
+        prefix = self.convert_ipv6_to_binary(prefix)
+        response = ""
         node = self.root
-        longest_match = None
-        for bit in binary_ip:
-            if bit in node.children:
-                node = node.children[bit]
-                if node.is_end_of_prefix:
-                    longest_match = node.prefix
-            else:
-                break
-        return longest_match
+        iterations = 0
+        for char in prefix:
+            iterations+=1
+            if char not in node.children:
+                if response == "":
+                    return response, iterations
+                continue
+            response+=char
+            node = node.children[char]
+        while node.is_end == False:
+            iterations+=1
+            keys = list(node.children.keys())
+            response+=keys[0]
+            node = node.children[keys[0]]
+        return response, iterations
 
-    def _ip_to_binary(self, ip):
-        return '.'.join(f'{int(octet):08b}' for octet in ip.split('.'))
+    def convert_ipv6_to_binary(self, prefix):
+        try:
+            net_obj = ipaddress.ip_network(prefix, strict=False)
+            if isinstance(net_obj, ipaddress.IPv6Network):
+                return bin(int(net_obj.network_address))[2:].zfill(128)
+            return prefix
+        except ValueError:
+            try:
+                ip_obj = ipaddress.ip_address(prefix)
+                if isinstance(ip_obj, ipaddress.IPv6Address):
+                    return bin(int(ip_obj))[2:].zfill(128)
+                return prefix
+            except ValueError:
+                return prefix
+
+def linear_longest_prefix_match(prefixes, ipv4):
+    response = ""
+    best_match = 0
+    iterations = 0
+    for prefix in prefixes:
+        min_len = min(len(prefix), len(ipv4))
+        count = 0
+        for i in range(min_len):
+            iterations+=1
+            if prefix[i] == ipv4[i]:
+                count +=1
+        if count > best_match:
+            best_match = count
+            response = prefix
+    return response, iterations
+
 
 if __name__ == "__main__":
-	trie = Trie()
-	trie.insert("192.168.0.0/16")
-	trie.insert("192.168.1.0/24")
-	trie.insert("10.0.0.0/8")
-	ip = "192.168.1.100"
-	print(f"The longest prefix match of ip {ip} is: {trie.longest_prefix_match(ip)}")
+    trie = Trie()
+    networks = ["2001:db8::/32", "2001:db8:1234::/48", "192.168.0.0/16", "192.168.1.0/24", "10.0.0.0/8"]
+    for network in networks:
+        trie.insert(network)
+    ipv6 = "2001:db8:1234:5678::1"
+    response, iterations = trie.longest_prefix_match(ipv6)
+    print(f"Trie longest prefix match: {response if response else "Not found"}")
+    ipv4 = "192.168.1.100"
+    response, iterations = trie.longest_prefix_match(ipv4)
+    print(f"Trie longest prefix match: {response if response else "Not found"}")
+
+    ipv4_trie = Trie()
+    ipv4_networks = []
+    for _ in range(1000):
+        ip = f"{randrange(0, 256)}.{randrange(0, 256)}.{randrange(0, 256)}.{randrange(0, 256)}"
+        prefix = randrange(24, 31)
+        network = str(ipaddress.ip_network(f"{ip}/{prefix}", strict=False))
+        ipv4_networks.append(network)
+        ipv4_trie.insert(network)
+
+    ipv4 = "192.168.200"
+    start = time.time()
+    response, iterations = ipv4_trie.longest_prefix_match(ipv4)
+    print(f"Trie longest prefix match: {response if response else "Not found"} Time: {round((time.time() - start), 3)} seconds Iterations: {iterations}")
+    start = time.time()
+    response, iterations = linear_longest_prefix_match(ipv4_networks, ipv4)
+    print(f"Linear longest prefix match: {response if response else "Not found"} Time: {round((time.time() - start), 3)} seconds Iterations: {iterations}")
