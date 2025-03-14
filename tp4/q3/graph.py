bfe52d436880aa3ec4ef184c6df9921b1ca12ddb
flaviorv <frezendevaroto@gmail.com>
2025-03-14 20:40:30 -0300

    Depth-first search method. This change the adjacency list in each search step.

--- tp4/q3/graph.py
+++ tp4/q3/graph.py
@@ -14,10 +14,24 @@ class NonDirectionalGraph:
         self.adjacency_list[vertex1].append(vertex2)
         self.adjacency_list[vertex2].append(vertex1)
 
+    def _dfs(self, vertex, visited=None):
+        if visited is None:
+            visited = set()
+        print(vertex, end=" ")
+        visited.add(vertex)
+        for neighbor in self.adjacency_list[vertex]:
+            if neighbor not in visited:
+                self._dfs(neighbor, visited)
+
+    def dfs(self, vertex, visited=None):
+        self._dfs(vertex, visited)
+        print()
+
 if __name__ == "__main__":
     edges = [("A", "B"), ("A", "C"), ("B", "D"), ("C", "D"), ("D", "E")]
     graph = NonDirectionalGraph()
     for edge in edges:
         graph.add_edge(edge[0], edge[1])
-    for key in graph.adjacency_list:
-        print(key, graph.adjacency_list[key])
+    # for key in graph.adjacency_list:
+        # print(key, graph.adjacency_list[key])
+    graph.dfs("A")
