7834aa07dbeafac2bd0771c0ce89b02d1b155d63
flaviorv <frezendevaroto@gmail.com>
2025-03-15 10:04:29 -0300

    Now the client sends a message to the server too.

--- tp4/q4/tcp_server.py
+++ tp4/q4/tcp_server.py
@@ -8,21 +8,25 @@ def start_server():
         server_socket.bind((HOST, PORT))
         server_socket.listen()
         print(f"Server listening in {HOST}:{PORT}")
-        conn, addr = server_socket.accept()
-        with conn:
-            print(f"Connection established in {addr}")
-            conn.sendall(b"Hello client TCP!")
+        while True:
+            conn, addr = server_socket.accept()
+            with conn:
+                print(f"Connection established in {addr}")
+                conn.sendall(b"Hello client TCP!")
+                data = conn.recv(1024)
+                print(f"Message received from client: {data.decode()}")
 
 def start_client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((HOST, PORT))
         data = client_socket.recv(1024)
         print(f"Message received from server: {data.decode()}")
+        client_socket.sendall(b"Hello server TCP!")
 
 if __name__ == "__main__":
     import threading
     server_thread = threading.Thread(target=start_server, daemon=True)
     server_thread.start()
     import time
-    time.sleep(1)
+    time.sleep(3)
     start_client()
