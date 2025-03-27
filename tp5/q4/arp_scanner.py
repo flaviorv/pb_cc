5e7f5ccf2fa3ff4d8abe89b5f8f679488176a175
flaviorv <frezendevaroto@gmail.com>
2025-03-27 16:53:02 -0300

    Waiting time has been increased

--- tp5/q4/arp_scanner.py
+++ tp5/q4/arp_scanner.py
@@ -5,7 +5,7 @@ network = "192.168.0.0/24"
 arp_request = ARP(pdst=network)
 ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 packet = ether / arp_request
-responses, _ = srp(packet, timeout=2, verbose=False)
+responses, _ = srp(packet, timeout=5, verbose=False)
 
 print("Active devieces:")
 for res in responses:
