c28204e5dd7a0011386052e78807d15b7c57156e
flaviorv <frezendevaroto@gmail.com>
2025-03-27 14:04:43 -0300

    ARP request for all active devices on a network. The responses allow us to map the mac with the ip of the devices.

---
+++ tp5/q4/arp_scanner.py
@@ -0,0 +1,12 @@
+from scapy.all import ARP, Ether, srp
+
+network = "192.168.0.0/24"
+
+arp_request = ARP(pdst=network)
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+packet = ether / arp_request
+responses, _ = srp(packet, timeout=2, verbose=False)
+
+print("Active devieces:")
+for res in responses:
+    print(f"IP: {res[1].psrc}, MAC: {res[1].hwsrc}")
